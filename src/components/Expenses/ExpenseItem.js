209373a12789690ba387e418153ba860ac17cc2b
vishnuu96 <riyachowdary37@gmail.com>
2023-07-29 20:02:01 +0530

    usestate update management

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,24 +1,34 @@
-// import React from "react";
+import React, {useState }from "react";
 import Cardd from '../UI/Cardd';
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props)=> {
+
+  const [title, setTitle] = useState(props.title)
+
+  const [expense, setExpense] = useState(props.amount)
+
   const clickHandler=()=>{
-   console.log('clicked!!!')
+    setTitle('updated')
+   console.log(title)
   }
   const deleteItem =()=>{
    console.log('expense deleted') 
+ }
+  const updateExpense =()=>{
+   setExpense(`100`)
  }
   
 
   return (
     <Cardd className='expense-item'>
         <ExpenseDate date={props.date} />
-       <ExpenseDetails title={props.title} location={props.location} amount={props.amount} />
+       <ExpenseDetails title={title} location={props.location} amount={expense} />
        <button onClick={clickHandler}>change Title</button>
        <button onClick={deleteItem}>Delete</button>
+       <button onClick={updateExpense}>update</button>
     </Cardd>
   );
 }
