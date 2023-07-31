278c796f2e583060a1159bb6ce3ff69947512d6e
vishnuu96 <riyachowdary37@gmail.com>
2023-07-31 12:04:59 +0530

    added multiple states

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,21 +1,34 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './ExpenseForm.css'
 const ExpenseForm = () => {
+ 
+  const [enteredTitle, setEnteredTitle] = useState('')
+  const [enteredAmount, setEnteredAmount] = useState('')
+  const [enteredDate, setEnteredDate] = useState('')
+  const titleEventHandler=(event)=>{
+    setEnteredTitle(event.target.value)
+  }
+  const amountEventHandler=(event)=>{
+    setEnteredAmount(event.target.value)
+  }
+  const dateEventHandler=(event)=>{
+    setEnteredDate(event.target.value)
+  }
   return (
     <div>
       <form>
         <div className='expense-new__controls'>
           <div className='expense-new__control'>
             <label htmlFor="title">Expense title</label>
-            <input type="text" id="title" />
+            <input type="text" id="title" value={enteredTitle} onChange={titleEventHandler}/>
           </div>
           <div className='expense-new__control'>
             <label htmlFor="amount">Expense amount</label>
-            <input type="number" id="amount" />
+            <input type="number" id="amount" value={enteredAmount} onChange={amountEventHandler}/>
           </div>
           <div className='expense-new__control'>
             <label htmlFor="date">date</label>
-            <input type="date" id="date" />
+            <input type="date" id="date" value={enteredDate} onChange={dateEventHandler} />
           </div>
          
         </div>
