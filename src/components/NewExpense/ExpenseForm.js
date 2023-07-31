04c001d83c0866b1be22c5eeeda20db5c8f01f1d
vishnuu96 <riyachowdary37@gmail.com>
2023-07-31 16:58:50 +0530

    form submission

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -5,18 +5,71 @@ const ExpenseForm = () => {
   const [enteredTitle, setEnteredTitle] = useState('')
   const [enteredAmount, setEnteredAmount] = useState('')
   const [enteredDate, setEnteredDate] = useState('')
+
+  // all states in one state
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle:'',
+  //   enteredAmount:'',
+  //   enteredDate:'',
+  // });
+
   const titleEventHandler=(event)=>{
     setEnteredTitle(event.target.value)
+
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle:event.target.value,
+    // })
+    // better approach would be
+    // setUserInput((prevState)=>{
+    // return  {...prevState,
+    //   enteredTitle:event.target.value,
+    // }
+
+    // })
   }
   const amountEventHandler=(event)=>{
     setEnteredAmount(event.target.value)
+
+    // setUserInput({
+    //   ...userInput,
+    //  enteredAmount:event.target.value,
+    // })
+    // setUserInput((prevState)=>{
+    //   return  {...prevState,
+    //     enteredAmount:event.target.value,
+    //   }
+  
+    //   })
   }
   const dateEventHandler=(event)=>{
     setEnteredDate(event.target.value)
+
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate:event.target.value,
+    // })
+    // setUserInput((prevState)=>{
+    //   return  {...prevState,
+    //     enteredDate:event.target.value,
+    //   }
+  
+    //   })
   }
+
+  const addExpense=(e)=>{
+    e.preventDefault()
+   const newExpense={
+    title:enteredTitle,
+    amount:enteredAmount,
+    date:enteredDate
+   }
+   console.log(newExpense)
+  }
+
   return (
     <div>
-      <form>
+      <form action={addExpense}>
         <div className='expense-new__controls'>
           <div className='expense-new__control'>
             <label htmlFor="title">Expense title</label>
@@ -33,7 +86,7 @@ const ExpenseForm = () => {
          
         </div>
         <div className='expense-new__actions'>
-            <button type="submit">Add</button>
+            <button type="submit" >Add</button>
           </div>
       </form>
     </div>
