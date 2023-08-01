486cb1bb8d2d35dafbdd88571d5fc1643370f97f
vishnuu96 <riyachowdary37@gmail.com>
2023-08-01 20:48:16 +0530

    added a filter which doenst work

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React,{useState} from 'react'
 import ExpenseItem from './ExpenseItem'
 import './Expenses.css';
 import Cardd from '../UI/Cardd';
+import ExpenseFilter from './ExpenseFilter';
 
 
 const Expenses=(props)=> {
+
+  const [filteredYear, setFilteredYear] = useState('2021');
+  const filterChangeHandler=(selectedYear)=>{
+      setFilteredYear(selectedYear)
+  }
   return (
     <Cardd className='expenses'>
+      <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
        {props.items.map((exp)=>{
       return <ExpenseItem id={exp.id}title={exp.title} amount={exp.amount} date={exp.date} location={exp.location} />
     
