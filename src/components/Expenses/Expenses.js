92fb0522e6714377a89ba64ba9c24ae051ccbb20
vishnuu96 <riyachowdary37@gmail.com>
2023-08-02 12:49:19 +0530

    conditional rendering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,15 +15,34 @@ const Expenses=(props)=> {
     return expense.date.getFullYear().toString()===filteredYear
   })
 
+  let expensesContent = <p className='content'>No expenses found</p>
+
+  if(filteredItems.length === 1){
+    expensesContent =  filteredItems.map((exp)=>{
+      return <><ExpenseItem key={exp.id}title={exp.title} amount={exp.amount} date={exp.date} location={exp.location} />
+      <p className='content'>Only single Expense here Please add more</p>
+      </> 
+     })
+  }
+  else if(filteredItems.length > 1) {
+    expensesContent =  filteredItems.map((exp)=>{
+      return <ExpenseItem key={exp.id}title={exp.title} amount={exp.amount} date={exp.date} location={exp.location} />
+     })
+  }
+
   
   return (
     <Cardd className='expenses'>
       <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-      {
-       filteredItems.map((exp)=>{
-      return <ExpenseItem key={exp.id}title={exp.title} amount={exp.amount} date={exp.date} location={exp.location} />
-    
-    })}
+      {/* {filteredItems.length===0 && <p>No expenses   found</p> }
+      { filteredItems.length > 0 &&
+        filteredItems.map((exp)=>{
+          return <ExpenseItem key={exp.id}title={exp.title} amount={exp.amount} date={exp.date} location={exp.location} />
+         })
+      } */}
+
+      {expensesContent}
+      
     </Cardd>
   )
 }
